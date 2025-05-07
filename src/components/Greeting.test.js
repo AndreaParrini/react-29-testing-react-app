a144a9a8931d8b364dbb0c33e59838a7efbf88f7
Andrea.Parrini.Lavoro <andrea.parrini26@gmail.com>
2025-05-07 10:26:50 +0200

    add test suits with describe

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,16 +1,19 @@
 import { render, screen } from "@testing-library/react"
 import Greeting from "./Greeting"
 
-test('renders Hello World as a text', ()=>{
-    // Arrange
-    render(<Greeting />);
+describe('Greeting Component', ()=>{
+    test('renders Hello World as a text', ()=>{
+        // Arrange
+        render(<Greeting />);
 
-    //Act
-    //... nothing
+        //Act
+        //... nothing
 
-    // Assert
-    // in questo caso se non aggiumao il ! fallirebbe il test, possiamo o aggiungere il ! 
-    // oppure aggiungere un parametro in più dopo la stringa {exact: false}, di default è sempre true.
-    const helloWorldElement= screen.getByText('Hello World!');
-    expect(helloWorldElement).toBeInTheDocument();
+        // Assert
+        // in questo caso se non aggiumao il ! fallirebbe il test, possiamo o aggiungere il ! 
+        // oppure aggiungere un parametro in più dopo la stringa {exact: false}, di default è sempre true.
+        const helloWorldElement= screen.getByText('Hello World!');
+        expect(helloWorldElement).toBeInTheDocument();
+    })
 })
+
