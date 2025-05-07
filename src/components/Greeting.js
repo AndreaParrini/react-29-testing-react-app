68ee37ffca397d7bd9785754ff66fc96c2f007b6
Andrea.Parrini.Lavoro <andrea.parrini26@gmail.com>
2025-05-07 10:58:24 +0200

    add tests of greeting components

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,8 +1,18 @@
+import { useState } from "react";
+
 const Greeting = () => {
+    const [changedText, setChengedText] = useState(false);
+
+    function handleClick(){
+        setChengedText(true);
+    }
+
     return(
         <div>
             <h2>Hello World!</h2>
-            <p>It's good to see you!</p>
+            {!changedText && <p>It's good to see you!</p>}
+            {changedText && <p>Changed!</p>}
+            <button onClick={handleClick}>Changed Text!</button>
         </div>
     );
 };
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,4 +1,6 @@
-import { render, screen } from "@testing-library/react"
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
 import Greeting from "./Greeting"
 
 describe('Greeting Component', ()=>{
@@ -14,6 +16,42 @@ describe('Greeting Component', ()=>{
         // oppure aggiungere un parametro in più dopo la stringa {exact: false}, di default è sempre true.
         const helloWorldElement= screen.getByText('Hello World!');
         expect(helloWorldElement).toBeInTheDocument();
+    });
+
+    test('renders good to see you if the button was NOT clicked', ()=>{
+        render(<Greeting />);
+
+        const paragraphElement = screen.getByText("good to see you", {exact: false});
+        expect(paragraphElement).toBeInTheDocument();
+    });
+
+    test('renders Changed! if the button was clicked', ()=>{
+        // Arrange
+        render(<Greeting />);
+
+        // Act
+        // se si usa la versione 14+ della libreria user-event si deve usare async nella funzione di test e poi await quando si usa userEvent.click()
+        const buttonElement= screen.getByRole('button');
+        userEvent.click(buttonElement);
+
+        // Assert
+        const paragraphElement = screen.getByText("Changed!");
+        expect(paragraphElement).toBeInTheDocument();
+    });
+
+    test('no renders "good to see you" if the button was clicked', ()=>{
+        // Arrange
+        render(<Greeting />);
+
+        // Act
+        // se si usa la versione 14+ della libreria user-event si deve usare async nella funzione di test e poi await quando si usa userEvent.click()
+        const buttonElement= screen.getByRole('button');
+        userEvent.click(buttonElement);
+
+        // Assert
+        // con queryByText, se non trova l'elemente restituisce nulla e cosi poi dopo posso controllare se è nulla.
+        const paragraphElement = screen.queryByText("good to see you", {exact: false});
+        expect(paragraphElement).toBeNull();
     })
 })
 
