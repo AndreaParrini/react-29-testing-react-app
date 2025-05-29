9036d5916dec991fc4beade1447f155d3053be17
Andrea <andrea.parrini@zucchetti.it>
2025-05-29 13:36:12 +0200

    pippo

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -7,7 +7,7 @@ describe('Async component', () => {
         window.fetch.mockResolvedValueOnce({
             json: async () => [{id: 'p1', title: 'first post'}]
         })
-        render(<Async />);
+        /* render(<Async />); */
 
         // si usa find qui perche c'è una richiesta http e in questo caso gli elementi del list item non sono subito disponibili
         // in questo modo si crea una promessa e si aspetta che venga risolta
